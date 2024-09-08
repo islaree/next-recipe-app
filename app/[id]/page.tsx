c56086ceb10052e13febaf17086711f1cdbdc5df
daxchx <daxchx.v1@gmail.com>
2024-09-08 17:05:30 +0900

    remove: unnecesary component

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import Link from "next/link";
-import AddMeal from "@/components/add-meal";
 import Meals from "@/components/meals";
 
 export default function MenuPage({ params }: { params: { id: string } }) {
   return (
     <>
       <Link href="/">← back</Link>
-      <Meals id={params.id} />
-      <div className="py-4">
-        <AddMeal id={params.id} />
-      </div>
+      <Meals />
     </>
   );
 }
