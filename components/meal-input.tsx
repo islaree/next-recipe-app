6bf089e04f1cb0c1135feb4396d826d937bec4c5
daxchx <daxchx.v1@gmail.com>
2024-09-08 13:15:17 +0900

    fix: type error

--- components/meal-input.tsx
+++ components/meal-input.tsx
@@ -1,12 +1,12 @@
-import { Meal } from "@/types/meal";
+import { MealType } from "@/types/meal";
 import { Input } from "./ui/input";
 
 export default function MealInput({
   meal,
   onUpdate,
 }: {
-  meal: Meal;
-  onUpdate: (newMeal: Meal) => void;
+  meal: MealType;
+  onUpdate: (newMeal: MealType) => void;
 }) {
   return (
     <Input
