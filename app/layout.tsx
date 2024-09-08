5523b218e12346df7226658ebe498eb2c06318a2
daxchx <daxchx.v1@gmail.com>
2024-09-08 14:01:30 +0900

    feat: add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} max-w-2xl p-6 mx-auto`}>
+      <body className={`${inter.className} max-w-2xl px-6 pt-6 pb-24 mx-auto`}>
+        <Navbar />
         {children}
       </body>
     </html>
