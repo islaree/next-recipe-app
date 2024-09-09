94580d3365af79cccdee10e6183bf31e64009bcc
daxchx <daxchx.v1@gmail.com>
2024-09-09 23:02:29 +0900

    feat: add ingredients store

--- store/store.ts
+++ store/store.ts
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 
 import { DishType } from "@/types/dish";
+import Ingredients from "@/components/ingredients";
+import { IngredientType } from "@/types/ingredient";
 
 type DishState = {
   dishes: DishType[];
@@ -8,6 +10,14 @@ type DishState = {
   get: (id: string, title: string) => DishType[] | [];
   delete: (id: string) => void;
   getAll: () => DishType[] | undefined;
+};
+
+type IngredientState = {
+  ingredients: IngredientType[];
+  add: (ingredient: IngredientType) => void;
+  get: (id: string) => IngredientType[] | [];
+  delete: (id: string) => void;
+  update: (ingredient: IngredientType) => void;
 };
 
 export const useDishStore = create<DishState>((set, get) => ({
@@ -23,5 +33,27 @@ export const useDishStore = create<DishState>((set, get) => ({
   },
   delete: (id) => {
     set((state) => ({ dishes: state.dishes.filter((d) => d.id !== id) }));
+  },
+}));
+
+export const useIngredientStore = create<IngredientState>((set, get) => ({
+  ingredients: [],
+  add: (ingredient) =>
+    set((state) => ({ ingredients: [...state.ingredients, ingredient] })),
+  get: (id) => {
+    const state = get();
+    return state.ingredients.filter((i) => i.dish_id === id);
+  },
+  delete: (id) => {
+    set((state) => ({
+      ingredients: state.ingredients.filter((i) => i.id !== id),
+    }));
+  },
+  update: (ingredient) => {
+    set((state) => ({
+      ingredients: state.ingredients.map((i) =>
+        i.id === ingredient.id ? ingredient : i
+      ),
+    }));
   },
 }));
